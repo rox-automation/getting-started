eaf8101aa002f7f8b2d3047f6a00c7e8d9f0dc97
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2024-11-04 21:38:35 +0000

    change display

--- scratch/virtual_can_sender.py
+++ scratch/virtual_can_sender.py
@@ -20,7 +20,7 @@ with UdpMulticastBus(channel=VCAN_IP) as bus:
             msg = can.Message(
                 arbitration_id=0x123, data=[counter], is_extended_id=False
             )
-            print(f"Sending message: {msg}")
+            print(f"id: {msg.arbitration_id:#x} data: {msg.data.hex()}")
             bus.send(msg)
             time.sleep(1)
             counter += 1
