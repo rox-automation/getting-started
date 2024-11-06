de3e6f6c6d83bd1b8465ff5dee07c1f2f4cc5ee9
Jelle <j.driessen@finefield.nl>
2024-11-06 12:08:14 +0000

    update classes

--- examples/104_classes.py
+++ examples/104_classes.py
@@ -1,25 +1,36 @@
 class Car:
 
+    max_weight = 1100
+
     def __init__(self, name: str, weight: float = 1000) -> None:
         self.name = name
         self.weight = weight
 
     def add_cargo(self, cargo_kg: float) -> None:
         self.weight = self.weight + cargo_kg
+        if self.weight > self.max_weight:
+            raise ValueError("Too much cargo!")
 
     def __str__(self) -> str:
-        return f"Class Car [{self.name}] {self.weight}"
+        return f"{self.__class__.__name__} [{self.name}] {self.weight}"
 
 
-jevs_car = Car("jev")
-jelles_car = Car("jelle", 2500)
+class Truck(Car):
 
-print(jevs_car.name, jevs_car.weight)
-print(jelles_car)
+    max_weight = 5000
 
 
-for idx in range(10):
-    jevs_car.add_cargo(100)
-    print(jevs_car)
+cars = [Truck("jev"), Car("jelle", 2500)]
+
+for car in cars:
+    print(car)
 
 
+jevs_car = cars[0]
+
+try:
+    for idx in range(100):
+        print(idx, jevs_car.weight)
+        jevs_car.add_cargo(100)
+except ValueError:
+    print("no more cargo please...")
